b1f2513674b8bab6aebe51fe69a091187683e01a
dfsdanimaulana <danimaulana9f@gmail.com>
2021-07-04 00:42:36 +0700

    selesai dashboard

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const story_container = document.querySelector('.story-container')
+const story = document.querySelector('.story-container').innerHTML
+
+
+for (let i = 0; i <= 8; i++) {
+    story_container.innerHTML += story
+}
+
+const card_container = document.querySelector('.card-container')
+const card = document.querySelector('.card-container').innerHTML
+for (let i = 1; i <= 2; i++) {
+    card_container.innerHTML += card
+}
